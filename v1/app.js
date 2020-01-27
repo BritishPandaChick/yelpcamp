d32b709f4f9f3d0f509d4b7c7383ec393ebc0387
BritishPandaChick <sbartley1080@gmail.com>
2020-01-27 17:11:27 -0500

    Add info to v1 README file. Add to campgrounds array in app.js. Add bootstrap classes to row class in campgrounds.file

--- v1/app.js
+++ v1/app.js
@@ -9,6 +9,12 @@ app.set("view engine", "ejs");
 var campgrounds = [
   {name: "Salmon Creek", image: "https://cdn.pixabay.com/photo/2017/06/17/03/17/gongga-snow-mountain-2411069__340.jpg"},
   {name: "Granite Hill", image: "https://cdn.pixabay.com/photo/2016/11/18/15/27/camping-1835352__340.jpg"},
+  {name: "Mountain Goat's Rest", image: "https://cdn.pixabay.com/photo/2016/01/26/23/32/camp-1163419__340.jpg"},
+  {name: "Salmon Creek", image: "https://cdn.pixabay.com/photo/2017/06/17/03/17/gongga-snow-mountain-2411069__340.jpg"},
+  {name: "Granite Hill", image: "https://cdn.pixabay.com/photo/2016/11/18/15/27/camping-1835352__340.jpg"},
+  {name: "Mountain Goat's Rest", image: "https://cdn.pixabay.com/photo/2016/01/26/23/32/camp-1163419__340.jpg"},
+  {name: "Salmon Creek", image: "https://cdn.pixabay.com/photo/2017/06/17/03/17/gongga-snow-mountain-2411069__340.jpg"},
+  {name: "Granite Hill", image: "https://cdn.pixabay.com/photo/2016/11/18/15/27/camping-1835352__340.jpg"},
   {name: "Mountain Goat's Rest", image: "https://cdn.pixabay.com/photo/2016/01/26/23/32/camp-1163419__340.jpg"}
 ];
 
